b28f2f53a0be85be518e0c1c68984988a8d69966
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-09 17:02:19 +0700

    Update pricingmodel.py

--- pricingmodel.py
+++ pricingmodel.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import pandas as pd
 from datetime import time
+import numpy as np
 
-st.title('pricing model')
+st.title('PRICING MODEL')
+
+chart_data = pd.DataFrame(
+   {
+       "col1": list(range(20)) * 3,
+       "col2": np.random.randn(60),
+       "col3": ["A"] * 20 + ["B"] * 20 + ["C"] * 20,
+   }
+)
+
+st.bar_chart(chart_data, x="col1", y="col2", color="col3")
