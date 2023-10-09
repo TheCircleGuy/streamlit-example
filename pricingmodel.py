3df4b65c3938759f63134c113fe603cefb36e1a0
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-09 16:44:34 +0700

    Update pricingmodel.py

--- pricingmodel.py
+++ pricingmodel.py
@@ -1,28 +1,7 @@
 import streamlit as st
-import numpy as np
-import plotly.figure_factory as ff
+from datetime import time
 
-
-#PRICING MODEL FOR POS SERVICE
-
-
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
-
-
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-        hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
+appointment = st.slider(
+    "Schedule your appointment:",
+    value=(time(11, 30), time(12, 45)))
+st.write("You're scheduled for:", appointment)
