841ed85eeb91aa7d94c3391bc0ea2241743830a0
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-11 17:09:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,7 @@ quantities = list(range(licenses[0], licenses[1] + 1))
 revenue = [price * q for q in quantities]
 
 fixedCost = [
-    MonthlyCost * q
-    for q in quantities
+    MonthlyCost * 12
 ]
 
 TotalManagerSalary = [
