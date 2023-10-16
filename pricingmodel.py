f19fad6dcc686c8ff4febb8071e25ef24dcd19b7
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-16 16:38:24 +0700

    Update pricingmodel.py

--- pricingmodel.py
+++ pricingmodel.py
@@ -71,10 +71,227 @@ with tab1:
     sales = st.slider('', 0, 200, (1, 30), key="a10")
       
 with tab2:
+     # Create a table to display the listed variables
+    # table_data = {
+    #     'Inputs': ['Monthly Cost', 'Number of Projects per Manager', 'Manager Salary',
+    #                 'Projects per server', 'Server Cost',  'Commission Percentage',
+    #                 'Average Revenue Of License', 'Average Licenses per Client', 'Price to be sold',
+    #                 'Range of Licenses', 'Profit Target'],
+    #     'Value': [MonthlyCost, ProjPerManager, ManagerSalary, ProjPerServer, ServerCost,
+    #             Commision, avgTransValue, avgLicensePerClient, price, f"{sales[0]} to {sales[1]}", Profit]
+    # }
 
-   left_co, cent_co,last_co = st.columns(3)
-   with cent_co:
-   st.image("https://two.vn/sites/all/themes/vermont/img/logo.png", width=200)
+
+    # Calculate the revenue, fixed cost, variable cost, and total cost for each quantity
+    Sales = list(range(sales[0], (sales[1] + 1) ))
+    revenue = [(price * avgLicensePerClient * q + Commision * avgTransValue * avgTransPerStore * 12 * 30 * q) for q in Sales]
+    fixedCost = [MonthlyCost * 12 for q in Sales]
+    ExpectedProfits = [Profit * 1 for q in Sales]
+
+    # TotalManagerSalary = [
+    #     ManagerSalary * q if (q // avgLicensePerClient) % ProjPerServer == 0 else ManagerSalary * (q + 1)
+    #     for q in Sales
+    # ]
+
+    TotalManagerSalary = []
+    for q in Sales:
+        if q < 5:
+            TotalManagerSalary.append(ManagerSalary*12)
+        else:
+            if (q % ProjPerManager) > 0:
+                TotalManagerSalary.append((q // ProjPerManager + 1) * ManagerSalary*12)
+            else:
+                TotalManagerSalary.append((q // ProjPerManager) * ManagerSalary*12)
+
+    TotalServerCost = []
+    for q in Sales:
+        if q < 5:
+            TotalServerCost.append(ServerCost*12)
+        else:
+            if (q % ProjPerManager) > 0:
+                TotalServerCost.append((q // ProjPerServer + 1) * ServerCost*12)
+            else:
+                TotalServerCost.append((q // ProjPerServer) * ServerCost*12)  
+                
+    VariableCost = np.array(TotalManagerSalary) + np.array(TotalServerCost)
+
+    TotalCost = np.array(fixedCost) + np.array(VariableCost)
+
+    TargetCost = np.array(TotalCost) + np.array(ExpectedProfits)
+
+    EstimatedProfit = np.array(revenue) - np.array(TotalCost)
+
+
+    show_last_table = st.checkbox("Show/Hide Inputs Table", value=True)
+
+    # Show the last table only if the checkbox is checked
+    if show_last_table:
+        st.table(table_data)
+
+    # BreakEvenSales = None
+    # for q in range(len(Sales)):
+    #     if revenue[q] - TotalCost[q] >= 0:
+    #         BreakEvenSales = Sales[q]
+    #         break
+
+    # if BreakEvenSales is not None:
+    #     st.write("When Sales =", BreakEvenSales, "; Revenue =", revenue[q], "; Total Cost =", TotalCost[q])
+    # else:
+    #     st.write("No Break-Even Sales found. Searching for optimal price and sales to break even.")
+
+    #     optimal_price = None
+    #     optimal_sales = None
+    #     found = False
+
+    #     for new_price in range(price, 10000, 100):  # Change the range and step size as needed
+    #         for new_sales in range(1, 1000):  # Change the range as needed
+    #             new_revenue = (new_price * avgLicensePerClient * new_sales + Commision * avgTransValue * avgTransPerStore * 12 * 30 * new_sales)
+    #             new_total_cost = (MonthlyCost * 12 + new_sales * ManagerSalary + new_sales * ServerCost)
+    #             if new_revenue - new_total_cost >= 0:
+    #                 optimal_price = new_price
+    #                 optimal_sales = new_sales
+    #                 found = True
+    #                 break
+    #         if found:
+    #             break
+
+    #     if optimal_price and optimal_sales:
+    #         st.write("Optimal Price:", optimal_price)
+    #         st.write("Optimal Sales Volume:", optimal_sales)
+    #     else:
+    #         st.write("No optimal price and sales combination found to break even.")
+
+    # Check for Break-Even Sales
+    BreakEvenSales = None
+    for q in range(len(Sales)):
+        if revenue[q] - TotalCost[q] >= 0:
+            BreakEvenSales = Sales[q]
+            break
+
+    st.text("")
+    st.text("")
+    st.text("")
+
+    # Display loading section
+    with st.spinner("Wait A Sec, Dan!"):
+        if BreakEvenSales is not None:
+            # st.write("When Sales =", BreakEvenSales, "; Revenue =", revenue[q], "; Total Cost =", TotalCost[q])
+    # Target Sales, Target Revenue, Total Cost, Profit, Nearest  competitor, Diff between competitor [Speedpos] )
+            col1, col2, col3, col4= st.columns(4)
+            col1.metric("Target Sales", value = BreakEvenSales )
+            col2.metric("Target Revenue", value = revenue[q] )
+            col3.metric("Total Cost", value = TotalCost[q] )
+            col4.metric("Profit", value =  revenue[q] - TotalCost[q] )
+            st.divider()
+            col1, col2, col3, col4 = st.columns(4)
+            col1.metric("", "")
+            col2.metric("Nearest  competitor price", value = 2000)
+            col3.metric("Diff between competitor", value = 0 )
+            col4.metric("", "")
+
+            # col1, col2, col3 = st.columns(3)
+            # col1.metric("Target Sales", "70 °F", "1.2 °F")
+            # col2.metric("Wind", "9 mph", "-8%")
+            # col3.metric("Humidity", "86%", "4%")
+        
+        else:
+            # Find the 5 most optimal price and sales pairs to break even
+            st.write("No exact Break-Even Sales found. Finding 5 most optimal price and sales pairs to break even...")
+
+            optimal_pairs = []
+            max_combinations = 5  # You can change this to get more or fewer combinations
+
+            for p in range(500, 2500000, 100):
+                for s in range(1, 200):
+                    revenue = p * avgLicensePerClient * s + Commision * avgTransValue * avgTransPerStore * 12 * 30 * s
+                    fixed_cost = MonthlyCost * 12
+                    variable_cost = ManagerSalary * s if s < 5 else (s // ProjPerManager + 1) * ManagerSalary
+                    total_cost = fixed_cost + variable_cost
+                    expected_profit = revenue - total_cost
+                    if expected_profit >= Profit:
+                        optimal_pairs.append((p, s, revenue, total_cost))
+                        if len(optimal_pairs) >= max_combinations:
+                            break
+                if len(optimal_pairs) >= max_combinations:
+                    break
+
+            st.write("5 Most Optimal Price and Sales Pairs to Break Even:")
+            for i, pair in enumerate(optimal_pairs):
+                st.write(f"Option {i + 1}: Price: {pair[0]}, Sales: {pair[1]}, Revenue: {pair[2]}, Total Cost: {pair[3]}")
+
+
+    st.text("")
+    st.text("")
+    st.text("")
+
+
+    # Create a DataFrame to hold the data
+    data = pd.DataFrame({
+        'Quantity': Sales,
+        'Fixed Cost': fixedCost,
+        'Total Cost': TotalCost,
+        'Revenue': revenue,
+        'ServerCost': TotalServerCost,
+        #'Desired Profit': [Profit] * len(Sales) + TotalCost,
+        'Target Cost': TargetCost,
+        'Variable Cost': VariableCost,
+        'Estimated Profit': EstimatedProfit,
+
+    })
+
+    # Create a line chart for Fixed Cost, Total Cost, Target Cost, and Revenue
+    st.markdown('<div style="text-align: center;">Price - Volume - Sales Mix</div>', unsafe_allow_html=True)
+    st.text("")
+
+    st.line_chart( data=
+        data.set_index('Quantity')
+        [[
+            'Fixed Cost', 
+            'Total Cost', 
+            'Target Cost',
+            'Revenue',
+            # 'ServerCost',
+            # 'Variable Cost',
+            # 'Estimated Profit'
+        ]]
+            
+        )
+
+
+    data = pd.DataFrame({
+        'Quantity': Sales,
+        'Fixed Cost': fixedCost,
+        'Total Cost': TotalCost,
+        'Revenue': revenue,
+        'Estimated Profit': EstimatedProfit,
+        'Desired Profit': [Profit] * len(Sales),
+        'Variable Cost': VariableCost,
+        'Total Salary': TotalManagerSalary,
+        
+    })
+
+    st.text("")
+    st.text("")
+    st.text("")
+
+    show_last_table = st.checkbox("Click to see detailed ouputs")
+
+    # Show the last table only if the checkbox is checked
+    if show_last_table:
+        st.markdown('<div style="text-align: center;">Variable Table</div>', unsafe_allow_html=True)
+        st.text("")
+        st.table(data[[
+            'Quantity',
+            'Fixed Cost',
+            'Total Cost',
+            'Revenue',
+            'Estimated Profit',
+            'Desired Profit',
+            'Variable Cost',
+            'Total Salary',
+        ]])
+
+  
 
 with tab3:
    st.header("An owl")
