7945a5c58daa8e70938fed2087f86d795b044064
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-17 11:50:01 +0700

    Update pricingmodel.py

--- pricingmodel.py
+++ pricingmodel.py
@@ -281,7 +281,7 @@ data = pd.DataFrame({
 st.markdown('<div style="text-align: center;">Price - Volume - Sales Mix</div>', unsafe_allow_html=True)
 st.text("")
     
-st.line_chart( data=data.set_index('Quantity')[[ 'Fixed Cost',  'Total Cost',  'Target Cost', 'Revenue']])
+st.line_chart( data=data.set_index('Quantity')[[ 'Fixed Cost',  'Total Cost',  'Target Cost', 'Revenue']], y="Revenue")
 
 
 data = pd.DataFrame({
