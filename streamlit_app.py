5f8385aa30fe9874de7305f6fb26bd02466b30f5
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-17 11:20:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.write("TEST")
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["col1", "col2", "col3"])
 
 st.line_chart(
-   chart_data, x="colxx", y=["col2", "col3"], color=["#FF0000", "#0000FF"]  # Optional
+   chart_data, x="hahahha", y=["col2", "col3"], color=["#FF0000", "#0000FF"]  # Optional
 )
