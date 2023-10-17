40208fc6cb99363c0ae6de15245041fdd7c5a221
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-17 11:19:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.write("TEST")
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["colxx", "col2", "col3"])
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["col1", "col2", "col3"])
 
 st.line_chart(
    chart_data, x="colxx", y=["col2", "col3"], color=["#FF0000", "#0000FF"]  # Optional
