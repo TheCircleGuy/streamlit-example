cef716af55bca856f227cae186fffacba1d49ec6
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-18 14:19:03 +0700

    Update pricingmodel.py

--- pricingmodel.py
+++ pricingmodel.py
@@ -236,11 +236,12 @@ with st.spinner("Wait A Sec, Dan!"):
         col1, col2, col3, col4= st.columns(4)
         with col1:
             col1.metric("Target Sales ", value = BreakEvenSales)
-          
-        st.write("Revenue = (annual license price x sales x average number of stores) + (average number transactions x average transaction vale)")        
+            
+        st.markdown("<h4 style='text-align: center; color: #0d043b;'>Total Cost = Fixed Cost + Variable Cost || Fixed Cost = Monthly Cost x 12 || Variable Cost = Server Cost + Salaries</h4>", unsafe_allow_html=True)
+        st.markdown("<h4 style='text-align: center; color: #0d043b;'>Revenue = (annual license price x sales x average number of stores) + (average number transactions x average transaction vale)>", unsafe_allow_html=True)
         col2.metric("Target Revenue", value = revenue[q] )
         col3.metric("Total Cost", value = TotalCost[q] )
-        col3.text("Total Cost = Fixed Cost + Variable Cost || Fixed Cost = Monthly Cost x 12 || Variable Cost = Server Cost + Salaries")
+        ")
         col4.metric("Profit", value =  revenue[q] - TotalCost[q] )
         st.divider()
         col1, col2, col3, col4 = st.columns(4)
