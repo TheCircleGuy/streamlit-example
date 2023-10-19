c2a13fbe865a490e11bfc28e00aef02711992c68
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-19 16:42:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
-import pandas as pd
 
+# Define the URL for your logo image
+logo_url = "https://softr-prod.imgix.net/applications/98b27be8-5282-4168-9e63-2aa100dc7626/assets/50f0ea3a-c669-4683-919a-74c4bae9e283.png"
 
-htp="https://softr-prod.imgix.net/applications/98b27be8-5282-4168-9e63-2aa100dc7626/assets/50f0ea3a-c669-4683-919a-74c4bae9e283.png"
-st.image(htp, caption= 'logo', width=350)
+# Use HTML and CSS to create the navigation bar with the logo
+navbar_html = f"""
+<div style="background-color: blue; padding: 10px;">
+    <img src="{logo_url}" alt="Logo" style="height: 50px; width: 50px; float: left; margin-right: 10px;">
+    <h1 style="color: white;">Your App Name</h1>
+</div>
+"""
+
+# Render the navigation bar
+st.markdown(navbar_html, unsafe_allow_html=True)
+
+# Rest of your Streamlit app goes here
+# For example:
+st.title("Welcome to Your App")
+st.write("This is the content of your app.")
