160fb3e59e065a491e7cba6079974d8e973077e1
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-19 16:49:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,11 +8,13 @@ logo_url = "https://example.com/your-logo-image.png"
 
 # Use HTML and CSS to create the navigation bar with the logo and centered text
 navbar_html = f"""
-<div style="background-color: #08043d; padding: 10px; display: flex; align-items: center;">
-    <img src="{logo_url}" alt="Logo" style="height: 150px; width: 150px; margin-right: 10px;">
-    <h1 style="color: white; text-align: center; margin: 0;">Pricing Model</h1>
+<div style="background-color: #08043d; padding: 10px;">
+    <img src="{logo_url}" alt="Logo" style="height: 50px; width: 50px; float: left; margin-right: 10px;">
+    <h1 style="color: white; text-align: center;">Pricing Model</h1>
 </div>
 """
+
+
 
 # Render the navigation bar
 st.markdown(navbar_html, unsafe_allow_html=True)
