06019767c8e0e379893c453d5e418710fe3dfd50
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-19 16:43:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
-
+https://softr-prod.imgix.net/applications/98b27be8-5282-4168-9e63-2aa100dc7626/assets/50f0ea3a-c669-4683-919a-74c4bae9e283.png
 # Define the URL for your logo image
+
 logo_url = "https://softr-prod.imgix.net/applications/98b27be8-5282-4168-9e63-2aa100dc7626/assets/50f0ea3a-c669-4683-919a-74c4bae9e283.png"
 
 # Use HTML and CSS to create the navigation bar with the logo
 navbar_html = f"""
-<div style="background-color: blue; padding: 10px;">
-    <img src="{logo_url}" alt="Logo" style="height: 50px; width: 50px; float: left; margin-right: 10px;">
+<div style="background-color: #08043d; padding: 10px;">
+    <img src="{logo_url}" alt="Logo" style="height: 150px; width: 150px; float: left; margin-right: 10px;">
     <h1 style="color: white;">Your App Name</h1>
 </div> 
 """
