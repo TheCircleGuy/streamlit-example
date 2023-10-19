6ed008fd0cc86358305025b6d7aad48aa22c58e8
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-19 16:36:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
-from PIL import Image
-import json
-import streamlit.components.v1 as com
-from streamlit_lottie import st_lottie
+
 
 htp="https://raw.githubusercontent.com/djswoosh/Music-Recommendation-Engine-using-FMA-Dataset/main/1200px-The_Echo_Nest_logo.svg.png"
 st.image(htp, caption= 'logo', width=350)
