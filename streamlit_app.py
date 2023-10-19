255d08c12cafd0c121c2710ee928dacb8189f6b4
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-19 16:38:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 
 
-htp="https://raw.githubusercontent.com/djswoosh/Music-Recommendation-Engine-using-FMA-Dataset/main/1200px-The_Echo_Nest_logo.svg.png"
+htp="https://softr-prod.imgix.net/applications/98b27be8-5282-4168-9e63-2aa100dc7626/assets/50f0ea3a-c669-4683-919a-74c4bae9e283.png"
 st.image(htp, caption= 'logo', width=350)
