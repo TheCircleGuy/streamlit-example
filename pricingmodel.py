88ed82c27c073b04097e6c34a64b48bb27790768
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-10-26 11:11:42 +0700

    Update pricingmodel.py

--- pricingmodel.py
+++ pricingmodel.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.set_page_config(layout="wide")
+
 st.markdown("<h1 style='text-align: center; color: #080b54;'>Pricing Model</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; color: #0d043b;'>Target Medium Size Businesses: 10 -30 stores</h3>", unsafe_allow_html=True)
 
