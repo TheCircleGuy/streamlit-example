4e75986ad7704c8e4be8fb7e1fe5eb2cee5e4e02
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2023-11-05 23:49:12 +0700

    Update NewsletterDemo.py

--- NewsletterDemo.py
+++ NewsletterDemo.py
@@ -1,67 +1,91 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from bokeh.models.widgets import Div
+
 
 # Set the page configuration to 'wide'
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
+# Set the theme to dark
+
+# st.set_page_config(
+#     page_title="Dias - Cost Volume Profit Analysis",
+#     page_icon="🧊",
+#     initial_sidebar_state="expanded",
+#     menu_items={
+#         'website': 'https://www.extremelycoolapp.com/help',
+#         'Linkedin': "https://www.linkedin.com/in/svenroering/",
+#     }
+# )
+
+st.set_page_config(
+    page_title="Dias - Cost Volume Profit Analysis",
+    page_icon="🧊",
+
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.linkedin.com/in/svenroering/',
+        'Report a bug': "https://www.linkedin.com/in/svenroering/",
+        'About': "https://www.linkedin.com/in/svenroering/"
+    }
+)
+
+st.markdown('<img src="https://raw.githubusercontent.com/TanNguyenDiasAdvisors/PriceAnalysis/main/assets/banner.png" style="width:100%;" />', unsafe_allow_html=True)
+
+
 # Create a title and logo in the left corner
-st.markdown("""
-    <style>
-        .logo {
-            padding: 10px 0px;
-            margin: 0px;
-            text-align: left;
-        }
-        .title {
-            text-align: center;
-            font-size: 36px; /* Larger font size */
-        }
-        .inputs-header {
-            text-align: center;
-            font-size: 20px;
-        }
-    </style>
-    <div class="logo">
-        <img src="https://raw.githubusercontent.com/TheCircleGuy/streamlit-example/fbd4e7f51bfa5d98bb703b3ed81326164734da40/assets/logo.png" alt="Logo" width=100 height=100>
-    </div>
-    <h1 class="title">Cost Volume Profit Analysis</h1>
-    """, unsafe_allow_html=True)
+# st.markdown("""
+#     <style>
+#         .logo {
+#             padding: 10px 0px;
+#             margin: 0px;
+#             text-align: left;
+#         }
+#         .title {
+#             text-align: center;
+#             font-size: 36px; /* Larger font size */
+#         }
+#         .inputs-header {
+#             text-align: center;
+#             font-size: 20px;
+#         }
+#     </style>
+#     <div class="logo">
+#         <img src="https://raw.githubusercontent.com/TheCircleGuy/streamlit-example/fbd4e7f51bfa5d98bb703b3ed81326164734da40/assets/logo.png" alt="Logo" width=100 height=100>
+#     </div>
+#     <h1 class="title">Cost Volume Profit Analysis</h1>
+#     """, unsafe_allow_html=True)
+
+
 
 # Create space below the title
 st.write(" ")
 
-def cook_breakfast():
-    msg = st.toast('Gathering Data...')
-    time.sleep(1)
-    msg.toast('Preping...')
-    time.sleep(1)
-    msg.toast('Ready!', icon = "🥞")
-
-if st.button('See Demo'):
-    cook_breakfast()
-    
+st.markdown("<h1 style='text-align: center;'>Cost Volume Profit Analysis</h1>", unsafe_allow_html=True)
+st.write(" ")
+st.markdown("<h3 style='text-align: center;'>Update the Inputs below to determine your target monthly sales volume</h3>", unsafe_allow_html=True)
+st.write("")
 # Create four columns for input fields
 col1, col2, col3, col4 = st.columns(4)
 
 # Input fields in the first column
 with col1:
-    fixed_costs = st.number_input("Fixed Monthly Costs", value=100000, key="fixed_costs")
+    fixed_costs = st.number_input("Fixed Monthly Costs", value=2000, key="fixed_costs")*12
     
 # Input fields in the second column
 with col2:
-    variable_cost_per_sale = st.number_input("Variable Cost per Sale", value=603, key="variable_cost_per_sale")
+    variable_cost_per_sale = st.number_input("Variable Cost per Sale", value=200, key="variable_cost_per_sale")
     
 # Input fields in the third column
 with col3:
-    target_profit = st.number_input("Target Profit", value=50000, key="target_profit")
+    target_profit = st.number_input("Target Profit", value=30000, key="target_profit")
 
 # Input fields in the fourth column
 with col4:
-    price_to_be_sold = st.number_input("Price to be sold", value=999, key="price_to_be_sold")
+    price_to_be_sold = st.number_input("Price to be sold", value=350, key="price_to_be_sold")
 
+st.divider()
 # Create slider input for "Quantity"
-quantity_range = st.slider("Quantity", 100, 500, (100, 500))
+quantity_range = st.slider("Quantity", 100, 5000, (100, 500))
 
 # Calculate the optimal price to achieve the target profit
 
@@ -82,13 +106,27 @@ for i in range(len(sales)):
     if revenue_values[i] - target_price_values[i] >= 0:
         break_even_sales = sales[i]
         break
-        
 
+st.markdown("<div style='text-align: center; background-color: #f89d13; padding: 20px; border-radius: 10px;'><h3 style='color: #161638;'>Target Number of Sales</h3><h1 style='color: #161638;'>{}</h1></div>".format(break_even_sales), unsafe_allow_html=True)
+st.write("")
+st.divider()
+st.write("")
 
+# Line chart with vertical axis as price and horizontal axis as sales
+# Showing Total Cost, Target Price, and Revenue
+chart_data = pd.DataFrame({
+    'Sales': sales,
+    'Total Cost': total_cost_values,
+    'Total Cost + Profit  Target': target_price_values,
+    'Revenue': revenue_values
+})
 
-URL_STRING = "https://www.dias-advisors.com/"
+st.markdown("<h3 style='text-align: center; color: #f89d13;'>Y-axis = currency, X-axis = sales volume</h3>", unsafe_allow_html=True)
+st.line_chart(chart_data.set_index('Sales')[['Total Cost', 'Total Cost + Profit  Target', 'Revenue']])
 
-st.markdown(
-    f'<a href="{URL_STRING}" style="display: inline-block; padding: 12px 20px; background-color: #080a33; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Connect With Dias</a>',
-    unsafe_allow_html=True
-)
+URL_STRING = "https://www.linkedin.com/in/svenroering/"
+
+coll1, coll2, coll3, coll4, coll5 = st.columns(5)
+with coll3:
+    st.markdown(f'<a href="{URL_STRING}" style="display: inline-block; padding: 12px 20px; background-color: #f89d13; color: black; text-align: center; text-decoration: none; font-size: 15px; border-radius: 4px;"><strong>Connect With Dias</strong></a>', unsafe_allow_html=True
+    )
