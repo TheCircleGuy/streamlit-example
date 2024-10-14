52086faa93857d8c49d3c490107f8e4839814961
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2024-10-14 21:26:28 +0700

    Update NewsletterDemo.py

--- NewsletterDemo.py
+++ NewsletterDemo.py
@@ -1,3 +1,3 @@
 import streamlit.components.v1 as components
 
-components.iframe("https://thecircleguy.github.io/SmartyFinalVersion/", height=500)
+components.iframe("https://thecircleguy.github.io/SmartyFinalVersion/", height=720, width=1080)
